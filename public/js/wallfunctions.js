35ac2fe56585521b9d79d84cac5e5f62289aae39
sophiaboss11 <51395437+sophiaboss11@users.noreply.github.com>
2021-02-18 23:28:19 -0800

    stickynote button

--- public/js/wallfunctions.js
+++ public/js/wallfunctions.js
@@ -1,26 +1,30 @@
-//$(document).ready(function() {
-//    initializePage();
-//});
-//
-//function initializePage() {
+$(document).ready(function() {
+    initializePage();
+});
+
+// function initializePage() {
 //    $('#testjs').click(function(e) {
 //            $('.jumbotron h1').text("Javascript is connected");
 //            });
-//
-//    $("a.thumbnail").click(projectClick);
-//
-//}
-//function projectClick(e) { 
-//    // prevent the page from reloading      
-//    e.preventDefault();
-//    // In an event handler, $(this) refers to      
-//    // the object that triggered the event      
-//    $(this).css("background-color", "#7fff00");
-//}
 
-funtion addNotes(){
+//    $("a.thumbnail").click(projectClick);
+
+// }
+// function projectClick(e) {
+//    // prevent the page from reloading     
+//    e.preventDefault();
+//    // In an event handler, $(this) refers to     
+//    // the object that triggered the event     
+//    $(this).css("background-color", "#7fff00");
+// }
+
+function addNotes(){
     //var test = "stickynote";
-    var test = $("<i></i>").text("StickyNote"); 
-    $("adding").after(test);
+    var running = $("<p></p>").text("StickyNote"); 
+
+    $("button").after(running);
+    console.log("add notes called");
 }
+
+
 
